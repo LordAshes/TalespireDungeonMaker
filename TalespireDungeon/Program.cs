d3647433068bdc33b6407b9d0338ba494fbe2844
Lord Ashes <webtable@gmail.com>
2023-02-18 10:22:38 -0500

    Initial Version

---
+++ TalespireDungeon/Program.cs
@@ -0,0 +1,212 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LordAshes
+{
+    class Program
+    {
+        class Room
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public int w { get; set; }
+            public int h { get; set; }
+            public bool rotunda { get; set; }
+        }
+
+        class Direction
+        {
+            public float x { get; set; }
+            public float y { get; set; }
+        }
+
+        class Door
+        {
+            public int x { get; set; }
+            public int y { get; set; }
+            public Direction dir { get; set; }
+            public int type { get; set; }
+        }
+
+        public static Random rnd = new Random();
+
+        static void Main(string[] args)
+        {
+            Dictionary<string, object> json = JsonConvert.DeserializeObject<Dictionary<string, object>>(System.IO.File.ReadAllText(args[0]));
+            int minX = int.MaxValue;
+            int maxX = int.MinValue;
+            int minY = int.MaxValue;
+            int maxY = int.MinValue;
+
+            foreach (Room rect in JsonConvert.DeserializeObject<Room[]>(json["rects"].ToString()))
+            {
+                if (rect.x < minX) { minX = rect.x; }
+                if (rect.x+rect.w > maxX) { maxX = rect.x+rect.w; }
+                if (rect.y < minY) { minY = rect.y; }
+                if (rect.y+rect.h > maxY) { maxY = rect.y+rect.h; }
+            }
+            int deltaX = (int)(maxX - minX + 4);
+            int deltaY = (int)(maxY - minY + 4);
+
+            // Create map space
+            string[,] map = new string[deltaX, deltaY];  
+            
+            // Populate default map
+            for (int y=0; y<deltaY; y++)
+            {
+                for (int x = 0; x < deltaX; x++)
+                {
+                    if (x == 0 || y == 0 || x == (deltaX - 1) || y == (deltaY - 1))
+                    {
+                        map[x, y] = "-";
+                    }
+                    else
+                    {
+                        map[x, y] = "#";
+                    }
+                }
+            }
+
+            // Populate map space
+            foreach (Room rect in (Room[])JsonConvert.DeserializeObject<Room[]>(json["rects"].ToString()))
+            {
+                for (int dy = (rect.y - minY); dy < (rect.y + rect.h - minY); dy++)
+                {
+                    for (int dx = (rect.x - minX); dx < (rect.x + rect.w - minX); dx++)
+                    {
+                        map[dx + 2, dy + 2] = ".";
+                    }
+                }
+            }
+
+            // Populate doors
+            foreach (Door door in JsonConvert.DeserializeObject<Door[]>(json["doors"].ToString()))
+            {
+                map[door.x - minX + 2, door.y - minY + 2] = "O";
+            }
+
+            // Apply translations
+            string[] translations = System.IO.File.ReadAllLines("Translations.txt");
+            foreach (string translation in translations)
+            {
+                if (translation.Trim() != "" && !translation.Trim().StartsWith("'"))
+                {
+                    string pattern = translation.Substring(0, translation.IndexOf(":"));
+                    string replacement = translation.Substring(translation.IndexOf(":")+1);
+                    for (int y = 1; y < deltaY-1; y++)
+                    {
+                        for (int x = 1; x < deltaX-1; x++)
+                        {
+                            if (IsMatch(map, x, y, pattern))
+                            {
+                                if (!replacement.Contains("W"))
+                                {
+                                    Write(ref map, x, y, replacement);
+                                }
+                                else
+                                {
+                                    if(rnd.Next(1,100+1)>50)
+                                    {
+                                        Write(ref map, x, y, replacement);
+                                    }
+                                    else
+                                    {
+                                        rnd.Next(1,100);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            // Trim the border
+            for (int y = 0; y < deltaY; y++)
+            {
+                for (int x = 0; x < deltaX; x++)
+                {
+                    if (x == 0 || y == 0 || x == (deltaX - 1) || y == (deltaY - 1))
+                    {
+                        map[x, y] = "";
+                    }
+                }
+            }
+
+            // Draw translated map
+            DrawMap(map);
+
+            // Create slab data
+            SlabBuilder.CreateJSON(map);
+
+            DrawMap(map,@"D:\wwwroot\TS\map.txt");
+            Console.WriteLine("Done!");
+            Console.ReadLine();
+        }
+
+        public static bool IsMatch(string[,] map, int x, int y, string pattern)
+        {
+            int offset = 0;
+            for(int dy=y-1; dy<=(y+1); dy++)
+            {
+                for (int dx = x - 1; dx <=(x+1); dx++)
+                {
+                    if(pattern.Substring(offset,1) != "?" && map[dx,dy]!=pattern.Substring(offset,1))
+                    {
+                        return false;
+                    }
+                    offset++;
+                }
+            }
+            return true;
+        }
+
+        public static bool Write(ref string[,] map, int x, int y, string replacement)
+        {
+            int offset = 0;
+            for (int dy = y - 1; dy <= y + 1; dy++)
+            {
+                for (int dx = x - 1; dx <= x + 1; dx++)
+                {
+                    if (replacement.Substring(offset,1) != "?")
+                    {
+                        map[dx, dy] = replacement.Substring(offset,1);
+                    }
+                    offset++;
+                }
+            }
+            return true;
+        }
+
+        public static void DrawMap(string[,] map, string outputfile = "")
+        {
+            // Draw map
+            if (outputfile != "") { System.IO.File.WriteAllText(outputfile,""); }
+                for (int y = 0; y <= map.GetUpperBound(1); y++)
+            {
+                for (int x = 0; x <= map.GetUpperBound(0); x++)
+                {
+                    if (outputfile == "")
+                    {
+                        Console.Write(map[x, y]);
+                    }
+                    else
+                    {
+                        System.IO.File.AppendAllText(outputfile,map[x, y]);
+                    }
+                }
+                if (outputfile == "")
+                {
+                    Console.WriteLine();
+                }
+                else
+                {
+                    System.IO.File.AppendAllText(outputfile, "\r\n");
+                }
+            }
+        }
+    }
+}
---
+++ TalespireDungeon/SlabBuilder.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LordAshes
+{
+    public static class SlabBuilder
+    {
+        public class TileAsset
+        {
+            public int rotation;
+            public string center;
+            public string extents;
+        }
+
+        public static string[,] map; 
+
+        private static string Map(int x, int y)
+        {
+            try
+            { 
+                return map[x - 1, y - 1];
+            }
+            catch           
+            {
+                return "#";
+            }
+        }
+
+        public static void CreateJSON(string[,] mapData)
+        {
+            map = mapData;
+            Dictionary<string, List<TileAsset>> tiles = new Dictionary<string, List<TileAsset>>();
+            Console.WriteLine("   Analyzing Tiles...");
+            string[] recordTiles = new string[] { "┌", "┐", "└", "┘", "┴", "┬", "┤", "├", "2", "4", "1", "3", "W", ".", "#", "╫" };
+            for (int y = 1; y <= (map.GetUpperBound(1)+1); y++)
+            {
+                for (int x = 1; x <= (map.GetUpperBound(0)+1); x++)
+                {
+                    if (recordTiles.Contains(Map(x, y)))
+                    {
+                        string nguid = GetNGuid(Map(x, y));
+                        if (!tiles.ContainsKey(nguid)) { tiles.Add(nguid, new List<TileAsset>()); }
+                        tiles[nguid].Add(new TileAsset() { rotation = GetTileRotation(Map(x, y)), center = GetTilePosition(Map(x, y), x, y), extents = GetTileExtends(Map(x, y)) });
+                        if ("1234".Contains(Map(x, y)))
+                        {
+                            tiles[GetNGuid(".")].Add(new TileAsset() { rotation = GetTileRotation("."), center = GetTilePosition(".", x, y), extents = GetTileExtends(".") });
+                        }
+                        if ("#".Contains(Map(x, y)))
+                        {
+                            tiles[GetNGuid("╬")].Add(new TileAsset() { rotation = GetTileRotation("╬"), center = GetTilePosition("╬", x, y), extents = GetTileExtends("╬") });
+                        }
+                        if ("╫".Contains(Map(x, y)))
+                        {
+                            tiles[GetNGuid("╪")].Add(new TileAsset() { rotation = GetTileRotation("╪"), center = GetTilePosition("╪", x, y), extents = GetTileExtends("╪") });
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("   Writing Tiles JSON...");
+            System.IO.File.WriteAllText(@"D:\wwwroot\TS\layout.js", "create_payload =\r\n");
+            System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "[\r\n");
+            for (int n = 0; n < tiles.Count; n++)
+            {
+                Console.WriteLine("     Writing '" + tiles.ElementAt(n).Key + "' Tiles...");
+                System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "    {\r\n");
+                System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "        'nguid': '" + tiles.ElementAt(n).Key + "',\r\n");
+                System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "        'assets':\r\n");
+                System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "        [\r\n");
+                for (int t = 0; t < tiles.ElementAt(n).Value.Count; t++)
+                {
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "            {\r\n");
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "                'rotation': " + tiles.ElementAt(n).Value.ElementAt(t).rotation + ",\r\n");
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "                'bounds':\r\n");
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "                {\r\n");
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "                    'center': " + tiles.ElementAt(n).Value.ElementAt(t).center + ",\r\n");
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "                    'extents': " + tiles.ElementAt(n).Value.ElementAt(t).extents + "\r\n");
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "                }\r\n");
+
+                    if (t < tiles.ElementAt(n).Value.Count - 1)
+                    {
+                        System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "            },\r\n");
+                    }
+                    else
+                    {
+                        System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "            }\r\n");
+                    }
+                }
+                System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "        ]\r\n");
+                if (n < tiles.Count - 1)
+                {
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "    },\r\n");
+                }
+                else
+                {
+                    System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "    }\r\n");
+                }
+            }
+            System.IO.File.AppendAllText(@"D:\wwwroot\TS\layout.js", "]\r\n");
+        }
+
+        private static string GetNGuid(string tile)
+        {
+            switch (tile)
+            {
+                case "┌":
+                case "┐":
+                case "└":
+                case "┘":
+                    return "ff6b5a27-a041-4ba1-ad33-01d82c77bb34";
+                case "┴":
+                case "┬":
+                case "┤":
+                case "├":
+                    return "05b8b832-e457-4ea7-88d5-25fa44037f39";
+                case "2":
+                case "4":
+                case "1":
+                case "3":
+                    return "a9759b32-9ad0-4157-ba8f-7d7e73a1f00c";
+                case ".":
+                    return "cf6063bb-5c6e-4107-b3e9-9c0c5ac75768";
+                case "#":
+                case "╬":
+                case "╫":
+                case "╪":
+                    return "82b03fd9-1afb-463f-a3f8-2f30204d6561";
+                case "W":
+                    switch(Program.rnd.Next(1,5+1))
+                    {
+                        case 1:
+                            return "2fc1f203-3702-4126-8777-5f94067a0c9d";
+                        case 2:
+                            return "910bdbf7-1cdf-4601-929a-9c89c5eaa8be";
+                        case 3:
+                            return "f8fdaf2d-3b5b-47d8-8e1d-077994535798";
+                        case 4:
+                            return "f12afdd6-a171-4a24-b3db-935c05e7aaf9";
+                        default:
+                            return "d3ec0216-95e4-44cc-b076-74f39504166e";
+                    }
+                default:
+                    return "0b9f9061-a6e4-4126-829f-1deb43147f49";
+            }
+        }
+
+        private static int GetTileRotation(string tile)
+        {
+            switch (tile)
+            {
+                case "┌": return 12;
+                case "┐": return 0;
+                case "└": return 8;
+                case "┘": return 4;
+                case "┴": return 12;
+                case "├": return 0;
+                case "┬": return 4;
+                case "┤": return 8;
+                case "2": return 4;
+                case "1": return 4;
+                case "3": return 8;
+                case "4": return 8;
+                case "#": return 0;
+                case "╬": return 0;
+                case "╫": return 4;
+                case "╪": return 4;
+                case "W": return 12;
+                default: return 0;
+            }
+        }
+
+        private static string GetTilePosition(string tile, double x, double y)
+        {
+            switch (tile)
+            {
+                case "4":
+                    return "{'x': " + (x * -1) + ", 'y': 0.5, 'z': " + (y+0.5) + "}";
+                case "3":
+                    return "{'x': " + (x * -1) + ", 'y': 0.5, 'z': " + y + "}";
+                case "2":
+                    return "{'x': " + (x * -1) + ", 'y': 0.5, 'z': " + y + "}";
+                case "1":
+                    return "{'x': " + ((x * -1) + 0.5) + ", 'y': 0.5, 'z': " + y + "}";
+                case "#":
+                    return "{'x': " + (x * -1) + ", 'y': 0.5, 'z': " + y + "}";
+                case "╬":
+                    return "{'x': " + ((x * -1) + 0.5) + ", 'y': 0.5, 'z': " + y + "}";
+                case "╫":
+                    return "{'x': " + (x * -1) + ", 'y': 0.5, 'z': " + y + "}";
+                case "╪":
+                    return "{'x': " + (x * -1) + ", 'y': 0.5, 'z': " + (y + 0.5) + "}";
+                case "+":
+                    return "{'x': " + (x * -1) + ", 'y': 0.5, 'z': " + y + "}";
+                case "W":
+                    return "{'x': " + (x * -1) + ", 'y': 0, 'z': " + (y+1.0) + "}";
+                default:
+                    return "{'x': " + (x*-1) + ", 'y': 0, 'z': " + y + "}";
+            }
+        }
+
+        private static string GetTileExtends(string tile)
+        {
+            switch (tile)
+            {
+                case "W":
+                    return "{ 'x': 2, 'y': 1, 'z': 2}";
+                default:
+                    return "{ 'x': 1, 'y': 1, 'z': 1}";
+            }
+        }
+    }
+}
